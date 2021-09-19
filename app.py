54c8508500f7e18378559e2bb210ae08785a7ebe
Rishikesh-kumar-7258 <rishi7258prince@gmail.com>
2021-09-19 21:21:22 +0530

    Making admin page

--- app.py
+++ app.py
@@ -3,11 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///test.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///posts.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
-app = Flask(__name__)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -15,17 +13,17 @@ class Post(db.Model):
     content = db.Column(db.String(1500), nullable=False)
     time = db.Column(db.DateTime, default=datetime.utcnow)
     tag = db.Column(db.String(50), nullable=False)
-    # image = db.Column(db.)
 
     def __repr__(self):
         return f"{self.id}-{self.title}"
 
-@app.route("/")
-def Home():
-    return render_template("index.html")
+@app.route("/", methods=["GET", "POST"])
+def home():
+    Allpost = Post.query.all()
+    return render_template("index.html", Allpost=Allpost)
 
-@app.route("/admin", methods=["GET", "POST"])
-def admin():
+@app.route("/adminlog", methods=["GET", 'POST'])
+def adminsignin():
     if request.method == 'POST':
         title = request.form['title']
         tag = request.form['tag']
@@ -33,8 +31,15 @@ def admin():
         post = Post(title=title, tag=tag, content=content)
         db.session.add(post)
         db.session.commit()
-        redirect('/admin')
+        return redirect('/admin')
     return render_template("admin.html")
+
+@app.route("/admin", methods=["GET", "POST"])
+def admin():
+    if request.method == 'POST':
+        return redirect('/adminlog')
+    
+    return render_template("adminsignin.html")
 
 if __name__ == "__main__":
     db.create_all()
