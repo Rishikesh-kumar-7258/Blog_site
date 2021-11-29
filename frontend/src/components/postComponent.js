ac81dd3cf05d38a1bc491bcb1077b5599db15931
Rishikesh-kumar-7258 <rishi7258prince@gmail.com>
2021-11-29 11:38:52 +0530

    Basic structure formed

--- frontend/src/components/postComponent.js
+++ frontend/src/components/postComponent.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import {
+    Link
+} from 'react-router-dom';
 
 class PostSection extends Component {
     constructor(props) {
@@ -9,14 +12,16 @@ class PostSection extends Component {
 
     }
 
+    handleClick = (e) => {
+        console.log(e.target);
+    }
 
-    
 
     render() {
         return (
             <div className="col-md-8 p-3 col-12">
                 <div className="row">
-                    <Renderposts posts={this.state.posts} />
+                    <Renderposts posts={this.state.posts} onclick={this.handleClick} />
                 </div>
             </div>
         )
@@ -36,20 +41,22 @@ const Renderposts = (props) => {
         //         </div>
         //     )
         // })
-        array.map(e => {
+        array.map((e, i) => {
             return (
-                <div class="card col-12 col-md-6">
-                    <img src="https://source.unsplash.com/1600x900/?nature,water" class="card-img-top" alt="post"/>
-                    <div class="card-body">
-                        <h5 class="card-title fs-1 fw-bolder">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <div className="d-flex text-center">
-                            <div style={{flex: 1}}>Date</div>
-                            <div style={{flex: 1}}>Catergories</div>
-                            <div style={{flex: 1}}>Tags</div>
+                <Link to={`/post/${e}`} key={i}>
+                    <div className="card col-12 col-md-6">
+                        <img src="https://source.unsplash.com/1600x900/?nature,water" className="card-img-top" alt="post" />
+                        <div className="card-body">
+                            <h5 className="card-title fs-1 fw-bolder">Card title</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <div className="d-flex text-center">
+                                <div style={{ flex: 1 }}>Date</div>
+                                <div style={{ flex: 1 }}>Catergories</div>
+                                <div style={{ flex: 1 }}>Tags</div>
+                            </div>
                         </div>
                     </div>
-                </div>
+                </Link>
             )
         })
     )
---
+++ frontend/src/components/postview.jsx
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+class Post extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return(
+            <div>
+                This is post view
+            </div>
+        )
+    }
+}
+
+export default Post;
