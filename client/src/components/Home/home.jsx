b2344973837606f98d3aa12a1bf9900168718b70
Rishikesh-kumar-7258 <rishi7258prince@gmail.com>
2022-05-08 22:44:10 +0530

    made some changes

--- client/src/components/Home/home.jsx
+++ client/src/components/Home/home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import HeaderImage from "../../Images/headerImage.jpg";
+import "./home.css";
 
 const HomePage = () => {
   return (
     <div className="container-fluid">
       <div className="row">
         <div className="col-12">
-          
+          <div id="front">
+            <p className="display-1">Blogger Prince</p>
+          </div>
         </div>
       </div>
     </div>
