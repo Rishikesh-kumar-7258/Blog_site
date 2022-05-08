bca71cb8945c63be2c556f42666709ece828f128
Rishikesh-kumar-7258 <rishi7258prince@gmail.com>
2022-05-08 21:35:59 +0530

    working on the frontend part

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./components/Home/home";
+import CreatePost from "./components/posts/create";
+
+// importing bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 const App = () => {
-  return <div>Hello, World!</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage/>} />
+      <Route path="post/create" element={<CreatePost />} />
+    </Routes>
+  );
 };
 
 export default App;
---
+++ client/src/components/Home/home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import HeaderImage from "../../Images/headerImage.jpg";
+
+const HomePage = () => {
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-12">
+          
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
