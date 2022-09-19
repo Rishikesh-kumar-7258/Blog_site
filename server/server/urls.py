cdccb83342c5852b2853e8c41fa5957827c5bd9c
Rishikesh Kumar <69416888+Rishikesh-kumar-7258@users.noreply.github.com>
2022-09-19 21:31:36 +0530

    Delete server directory

--- server/server/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from rest_framework.authtoken import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('api.urls')),
-    path('api-token-auth', views.obtain_auth_token)
-]
